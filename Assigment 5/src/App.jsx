cc811e069284d76304cd8a701818a3abf28cf5cb
Akash <aakasgdev24@gmail.com>
2023-06-06 07:20:54 +0530

    assigment-4-added

---
+++ Assigment 5/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+import './App.css'
+import NewExpense from './components/newExpenses/NewExpense';
+import Expenses from './components/expenses/Expenses';
+
+import ExpensesFilter from './components/expenses/ExpenseFilter';
+const data = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(data);
+  const [openNewExpenses,setOpenNewExpenses]=useState(false);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+ 
+  const openNew=()=>{
+    setOpenNewExpenses(!openNewExpenses)
+  }
+  console.log("open",openNewExpenses)
+
+  return (
+    <div>
+      {
+        !openNewExpenses && <button onClick={() => setOpenNewExpenses(!openNewExpenses)}>Add New Expenses</button>
+      }
+      {
+        openNewExpenses && <NewExpense onAddExpense={addExpenseHandler} open={openNew} />
+
+      }
+
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+export default App
