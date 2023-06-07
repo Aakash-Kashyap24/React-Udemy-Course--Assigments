8fcee1e5da7853c6313299116ef78656135ffa7c
Akash <aakasgdev24@gmail.com>
2023-06-07 07:30:05 +0530

    assigment-6-added

---
+++ Assigment 6/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+import Input from './layout/Input';
+const App = () => {
+
+
+  return (
+    <div>
+      <Input />
+    </div>
+  );
+};
+export default App
